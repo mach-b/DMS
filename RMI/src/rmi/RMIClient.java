089792137a5c7b532c8871464b6db8ee74d620a9
mach-b <markburton@Marks-MacBook-Pro.local>
2014-05-21 12:59:31 +1200

    Added RMIServer, RMIClient

---
+++ RMI/src/rmi/RMIClient.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rmi;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author markburton
+ */
+public class RMIClient {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RMIClient client = new RMIClient();
+        client.connectServer();
+    }
+
+    private void connectServer() {
+        try {
+            Registry reg =  LocateRegistry.getRegistry("localhost", 1099);
+            //Registry reg =  LocateRegistry.getRegistry("127.0.0.1", 1099);
+            RMI rmi =  (RMI) reg.lookup("server");
+            System.out.println("Connected to server.");
+            String text = rmi.getString("Mark");
+            System.out.println(text);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
