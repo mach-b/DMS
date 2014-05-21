98690fc93a210877015f0faf8e9a2e1895930b5c
Cherry_Kerry <the.kerry.powell@gmail.com>
2014-05-22 11:35:01 +1200

    Added Java Docs

--- RMI/src/share/SharedFiles.java
+++ RMI/src/share/SharedFiles.java
@@ -1,6 +1,7 @@
 package share;
 
 import java.io.File;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -11,29 +12,52 @@ import java.util.Set;
 import rmi.SharedFilesRMI;
 
 /**
- *
- * @author kerrypowell
+ * Class that represents the files that can be shared from each application
+ * 
+ * @author Kerry Powell
+ * @version 1.0
  */
 public class SharedFiles extends UnicastRemoteObject implements SharedFilesRMI {
 
     Set<File> files;
     
-    public SharedFiles() throws RemoteException{
+    public SharedFiles() throws RemoteException {
+        
         super();
         files = new HashSet<>();
         registerRMI();
     }
     
+    /**
+     * Registers the class to be accessed via RMI
+     */
     private void registerRMI() {
+        
         Registry reg;
         try {
             reg = LocateRegistry.createRegistry(1099);
-            reg.bind(this.toString(), this);
+            reg.bind("SharedFiles", this);
             System.out.println("Server has started.");
         } catch (Exception ex) {
             System.err.println(ex);
         }    
     }
+    
+    /**
+     * Gets a remote instance of the shared files RMI interface from the given 
+     * ip address
+     * 
+     * @param ip of the machine that you want to get the interface from
+     * @return the RMI interface for shared files
+     * @throws RemoteException if the remote registry cannot be found
+     * @throws NotBoundException if the remote registry doesn't have an interface 
+     */
+    public static SharedFilesRMI getRemoteFiles(String ip) 
+            throws RemoteException, NotBoundException {
+        
+        Registry reg =  LocateRegistry.getRegistry(ip, 1099);
+        return (SharedFilesRMI) reg.lookup("SharedFiles");
+    }  
     
     @Override
     public String[] getFileNames() {
@@ -48,12 +72,12 @@ public class SharedFiles extends UnicastRemoteObject implements SharedFilesRMI {
         return names;
     }
     
-    @Override
-    public String toString() {
-        
-        return "SharedFiles";
-    }
-    
+    /**
+     * Add a file to the local shared files
+     * 
+     * @param file to be added to the list
+     * @return true if the file was added
+     */
     public boolean addFile(File file) {
         return files.add(file);
     }
